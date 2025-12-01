96ef4351cbed576c60cacc8ce454db0781344cb6
Calvin Leo <calvin.leo05@gmail.com>
2025-12-01 14:00:35 +0700

    No. 2 – Upgrading app with flow

--- app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
+++ app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.test_lab_week_12
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.RecyclerView
 import com.example.test_lab_week_12.model.Movie
 import com.google.android.material.snackbar.Snackbar
-import java.util.Calendar
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val movieAdapter by lazy {
@@ -36,20 +39,22 @@ class MainActivity : AppCompatActivity() {
             }
         )[MovieViewModel::class.java]
 
-        movieViewModel.popularMovies.observe(this) { popularMovies ->
-            val currentYear = Calendar.getInstance().get(Calendar.YEAR).toString()
-            movieAdapter.addMovies(
-                popularMovies
-                    .filter { movie ->
-                        movie.releaseDate?.startsWith(currentYear) == true
+        // Ganti LiveData observer dengan Flow collector
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    movieViewModel.popularMovies.collect { movies ->
+                        movieAdapter.addMovies(movies)
                     }
-                    .sortedByDescending { it.popularity }
-            )
-        }
+                }
 
-        movieViewModel.error.observe(this) { error ->
-            if (error.isNotEmpty()) {
-                Snackbar.make(recyclerView, error, Snackbar.LENGTH_LONG).show()
+                launch {
+                    movieViewModel.error.collect { error ->
+                        if (error.isNotEmpty()) {
+                            Snackbar.make(recyclerView, error, Snackbar.LENGTH_LONG).show()
+                        }
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.test_lab_week_12
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.test_lab_week_12.model.Movie
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 
 class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
@@ -13,15 +15,19 @@ class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel()
         fetchPopularMovies()
     }
 
-    val popularMovies: LiveData<List<Movie>>
-        get() = movieRepository.movies
+    private val _popularMovies = MutableStateFlow(emptyList<Movie>())
+    val popularMovies: StateFlow<List<Movie>> = _popularMovies
 
-    val error: LiveData<String>
-        get() = movieRepository.error
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> = _error
 
     private fun fetchPopularMovies() {
         viewModelScope.launch(Dispatchers.IO) {
-            movieRepository.fetchMovies()
+            movieRepository.fetchMovies().catch {
+                _error.value = "An exception occurred: ${it.message}"
+            }.collect {
+                _popularMovies.value = it
+            }
         }
     }
 }
